e9b66c168f223e3fe3e8ff385096641d1b1f8d1c
Kohxax <koharen0256@gmail.com>
2024-11-26 19:06:49 +0900

    <add> DatabaseManager class (WIP)

---
+++ src/main/java/dev/bokukoha/onJoinDynmapURL/DatabaseManager.java
@@ -0,0 +1,17 @@
+package dev.bokukoha.onJoinDynmapURL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    private Connection connection;
+
+    //create SQL table - WIP
+    public void initialize() {
+
+    }
+}
